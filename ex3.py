c905bb5a7dd83e85a50c752fdc58ddca1a14eae0
MoniaJ <monia.jankowiak@gmail.com>
2017-11-23 20:29:36 +0100

    Update ex3.py
    change a few of numbers to floats

--- ex3.py
+++ ex3.py
@@ -1,7 +1,7 @@
 print("I will now count my chicken:")
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100 - 25 * 3.0 % 4)
 
 print("Now I will count the eggs:")
 
@@ -11,7 +11,7 @@ print("Is it true that 3 + 2 < 5 - 7?")
 
 print(3 + 2 < 5 - 7)
 
-print("What is 3 + 2?", 3 + 2)
+print("What is 3 + 2?", 3.0 + 2)
 print("What is 5 - 7?", 5 - 7)
 
 print("Oh, that's why  it's False")
